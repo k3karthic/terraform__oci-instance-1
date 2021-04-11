cf2def95bc2c0145f0058c01379a2ead93c162a9
Karthic Kumaran <k3.karthic@live.com>
2021-04-11 13:08:59 +0000

    terraform format

--- main.tf
+++ main.tf
@@ -7,30 +7,30 @@ variable "image_id" {}
 provider "oci" {}
 
 resource "oci_core_network_security_group" "free" {
-    compartment_id = var.compartment
-    vcn_id = var.vcn_id
+  compartment_id = var.compartment
+  vcn_id         = var.vcn_id
 }
 
 resource "oci_core_instance" "free" {
-    availability_domain = var.ad
-    compartment_id = var.compartment
-    shape = "VM.Standard.E2.1.Micro"
+  availability_domain = var.ad
+  compartment_id      = var.compartment
+  shape               = "VM.Standard.E2.1.Micro"
 
-    create_vnic_details {
-        hostname_label = "free1"
-        subnet_id = var.subnet_id
+  create_vnic_details {
+    hostname_label = "free1"
+    subnet_id      = var.subnet_id
 
-        assign_public_ip = true
-        nsg_ids = [oci_core_network_security_group.free.id]
-    }
+    assign_public_ip = true
+    nsg_ids          = [oci_core_network_security_group.free.id]
+  }
 
-    metadata = {
-        ssh_authorized_keys = file("${path.module}/ssh/oracle.pub")
-    }
+  metadata = {
+    ssh_authorized_keys = file("${path.module}/ssh/oracle.pub")
+  }
 
-    source_details {
-        boot_volume_size_in_gbs = 50
-        source_type = "image"
-        source_id = var.image_id
-    }
+  source_details {
+    boot_volume_size_in_gbs = 50
+    source_type             = "image"
+    source_id               = var.image_id
+  }
 }
