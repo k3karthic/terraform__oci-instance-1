530be7bf01e767664de4eba97966e228fa0000a9
Karthic Kumaran <k3.karthic@protonmail.ch>
2021-04-15 14:43:49 +0000

    comments, refactor input variables

--- main.tf
+++ main.tf
@@ -1,11 +1,67 @@
+/*
+ * Variables
+ */
+
 variable "ad" {}
 variable "compartment" {}
+
+variable "hostname" {}
+
 variable "vcn_id" {}
 variable "subnet_id" {}
+
 variable "image_id" {}
+variable "image_os" {}
+
 variable "ydns_host" {}
 
+/*
+ * Providers
+ */
+
 provider "oci" {}
+
+/*
+ * Configuration
+ */
+
+//
+// Instance
+//
+
+resource "oci_core_instance" "free" {
+  availability_domain = var.ad
+  compartment_id      = var.compartment
+  shape               = "VM.Standard.E2.1.Micro"
+
+  create_vnic_details {
+    hostname_label = var.hostname
+    subnet_id      = var.subnet_id
+
+    assign_public_ip = true
+    nsg_ids          = [oci_core_network_security_group.free.id]
+  }
+
+  metadata = {
+    ssh_authorized_keys = file("${path.module}/ssh/oracle.pub")
+  }
+
+  source_details {
+    boot_volume_size_in_gbs = 50
+    source_type             = "image"
+    source_id               = var.image_id
+  }
+
+  freeform_tags = {
+    "openvpn_service" = "yes"
+    "os"              = var.image_os
+    "ydns_host"       = var.ydns_host
+  }
+}
+
+//
+// Network Security Group
+//
 
 resource "oci_core_network_security_group" "free" {
   compartment_id = var.compartment
@@ -43,35 +99,5 @@ resource "oci_core_network_security_group_security_rule" "openvpn" {
       min = 1194
       max = 1194
     }
-  }
-}
-
-resource "oci_core_instance" "free" {
-  availability_domain = var.ad
-  compartment_id      = var.compartment
-  shape               = "VM.Standard.E2.1.Micro"
-
-  create_vnic_details {
-    hostname_label = "free1"
-    subnet_id      = var.subnet_id
-
-    assign_public_ip = true
-    nsg_ids          = [oci_core_network_security_group.free.id]
-  }
-
-  metadata = {
-    ssh_authorized_keys = file("${path.module}/ssh/oracle.pub")
-  }
-
-  source_details {
-    boot_volume_size_in_gbs = 50
-    source_type             = "image"
-    source_id               = var.image_id
-  }
-
-  freeform_tags = {
-    "openvpn_service" = "yes"
-    "os" = "ubuntu"
-    "ydns_host" = var.ydns_host
   }
 }
